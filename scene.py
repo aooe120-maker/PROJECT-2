8a1efc8f0dbe4e3d15f9875c7870b42ffaf6f178
h4yase <4shout4@gmail.com>
2025-08-29 19:55:10 +0900

    scenes.lower()

--- scene.py
+++ scene.py
@@ -16,11 +16,9 @@ class Scene:
 
 def load_scenes(game):
 	scenes = []
-	# scenes 패키지 안의 모든 모듈을 탐색
 	package = "scenes"
 	for _, module_name, _ in pkgutil.iter_modules([package]):
 		module = importlib.import_module(f"{package}.{module_name}")
-		# 각 모듈에 필요한 속성이 있다고 가정
 		scene = Scene(
 			module.name,
 			module.req_like,
